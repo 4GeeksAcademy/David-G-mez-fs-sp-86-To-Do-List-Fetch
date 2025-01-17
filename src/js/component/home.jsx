6f8872d0dcba3ad73ffafe96610279398c411118
Davidgmzc <davidgomz.c13@gmail.com>
2025-01-17 13:23:47 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,76 @@
-import React from "react";
+import React,{ useEffect, useState } from 'react'
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function App() {
+  
+  const [state, setState] = useState([])
+  const [input, setInput] = useState("")
+  
+  useEffect(() => {
+    getTodoList();
+  }, [])
+  
+  const getTodoList = async () => {
+    const response = await fetch('https://playground.4geeks.com/todo/users/DavidGmzc', {
+      method: 'GET'
+    });
+    if (!response.ok){
+      console.error("Error al iniciar el to do list");
+      createUser();
+    }
+    console.log(response)
+    const {todos} = await response.json();
+    setState(todos);
+  }
+ 
+  const createUser = async () => {
+    const response = await fetch('https://playground.4geeks.com/todo/users/DavidGmzc', {
+      method: 'POST'
+    })
+    const data = await response.json()
+    console.log("usuario creado: ", data);
+  }
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  const addTodoItem = async () => {
+    await fetch('https://playground.4geeks.com/todo/todos/DavidGmzc', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ label: input, is_done: false })
+    })
+    await getTodoList();
+    setInput('');
+  }
+  
+  const deleteTodoItem = async (todoId) => {
+    await fetch(`https://playground.4geeks.com/todo/todos/${todoId}`, {
+      method: 'DELETE'
+    })
+    await getTodoList();
+  }
 
-export default Home;
+  return (
+    <>
+      <h1>To Do List</h1>
+      <br/>
+        <input value={input}  onChange={(e) => {
+          setInput(e.target.value)
+        }}></input>
+        <br/>
+        <button onClick={addTodoItem}>Agregar</button>
+        <br/>
+        <br/>
+      <ul>
+        {state.map((item, index) => {
+          return (<>
+            <li>
+              {item.label} <button className='btn' onClick={() => deleteTodoItem(item.id)}>x</button>
+            </li>
+          </>)
+        })}
+      </ul>
+      <p>{state.length} Items to do</p>
+    </>
+  )
+}
+export default App;
